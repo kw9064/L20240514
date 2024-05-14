e60e6eb72c85666a8ace345e7d58c90c8503bded
kw9064 <kw9064990531@gmail.com>
2024-05-14 09:49:55 +0900

    L20240514_1.1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,68 @@
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
 using namespace std;
 
+int main()
+{
+	int Map[10][10] =
+	{
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	};
+
+int PlayerX = 1;
+int PlayerY = 1;
+char Key = 0;
+
+	for (;;)
+	{
+		cin >> Key;
+		if (Key == 'w')
+		{
+			PlayerY--;
+		}
+		else if (Key == 's')
+		{
+			PlayerY++;
+		}
+		else if (Key == 'a')
+		{
+			PlayerX--;
+		}
+		else if (Key == 'd')
+		{
+			PlayerX++;
+		}
+
+	system("cls");
+	for (int Y = 0; Y < 10; ++Y)
+	{
+		for (int X = 0; X < 10; ++X)
+		{
+			if (PlayerX == X && PlayerY == Y)
+			{
+				cout << "P";
+			}
+			else if (Map[Y][X] == 0)
+			{
+				cout << " ";
+			}
+			else if (Map[Y][X] == 1)
+			{
+				cout << "*";
+			}
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
