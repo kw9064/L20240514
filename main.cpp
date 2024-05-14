848e848e428eea197e0d112f9e53425a725b0759
kw9064 <kw9064990531@gmail.com>
2024-05-14 11:25:15 +0900

    L20240514_1.3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <conio.h>
 #include <winsock.h>
+
 using namespace std;
 
 int main()
@@ -19,9 +20,9 @@ int main()
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	};
 
-int PlayerX = 1;
-int PlayerY = 1;
-char Key = 0;
+	int PlayerX = 1;
+	int PlayerY = 1;
+	char Key = 0;
 
 	for (;;)
 	{
@@ -43,26 +44,27 @@ char Key = 0;
 			PlayerX++;
 		}
 
-	system("cls");
-	for (int Y = 0; Y < 10; ++Y)
-	{
-		for (int X = 0; X < 10; ++X)
+		system("cls");
+		for (int Y = 0; Y < 10; ++Y)
 		{
-			if (PlayerX == X && PlayerY == Y)
+			for (int X = 0; X < 10; ++X)
 			{
-				cout << "P";
+				if (PlayerX == X && PlayerY == Y)
+				{
+					cout << "P";
+				}
+				else if (Map[Y][X] == 0)
+				{
+					cout << " ";
+				}
+				else if (Map[Y][X] == 1)
+				{
+					cout << "*";
+				}
 			}
-			else if (Map[Y][X] == 0)
-			{
-				cout << " ";
-			}
-			else if (Map[Y][X] == 1)
-			{
-				cout << "*";
-			}
+			cout << endl;
 		}
-		cout << endl;
-	}
 
-	return 0;
+		return 0;
+	}
 }
